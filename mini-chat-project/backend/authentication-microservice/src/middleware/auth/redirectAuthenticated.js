ff7bfa7a6045ed0a1b4059dcb42d385248bdcee5
RubenBenavidess <dario.benavides@hotmail.com>
2025-11-16 00:22:32 -0500

    Fixed Forbidden Access

--- mini-chat-project/backend/authentication-microservice/src/middleware/auth/redirectAuthenticated.js
+++ mini-chat-project/backend/authentication-microservice/src/middleware/auth/redirectAuthenticated.js
@@ -8,16 +8,17 @@ export default function redirectAuthenticated(req, res, next){
 
         let token;
         const headerValue = req.header(TOKEN_HEADER_KEY);
-        if (headerValue?.startsWith("Bearer ")) {
+        if(headerValue?.startsWith("Bearer ")) {
             token = headerValue.slice(7);
         }
 
-        if (validateToken(token)) {   
-            throw new Error("Forbidden Access");
-        }
-        return next();
+        validateToken(token);
+
+        throw new Error("Forbidden Access");
+        
 
     }catch(e){
+        if(e.message === "Forbidden Access") return next(e);    
         return next();
     }
 
