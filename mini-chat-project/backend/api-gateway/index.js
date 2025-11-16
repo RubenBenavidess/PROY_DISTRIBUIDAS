74b882877ec05aa45486377baf8597245c9e1684
Edverdesoto <edisondamian12@gmail.com>
2025-11-15 23:00:20 -0500

    Gateway

---
+++ mini-chat-project/backend/api-gateway/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
+const app = express();
+
+// Gateway port
+const PORT = process.env.G_PORT;
+
+// Microservice 
+const AUTH_SERVICE_URL = 'http://auth-api:3000';
+const MESSAGE_SERVICE_URL = 'http://message-management-microservice:8001';
+
+// Middlewares
+app.use(helmet()); // Security headers
+app.use(cors()); // Enable CORS for all routes
+app.use(morgan('dev')); // Logging
+
+const limiter =rateLimit({
+    windowMs: 15 * 60 * 1000, //15 minutes window
+    max: 100, // each IP has 100 requests per windowMs
+    message: 'Demasiadas solicitudes desde esta IP.\n Intente de nuevo más tarde.',
+    standardHeaders: true, // Return rate limit info in the headers
+    legacyHeaders: false, // Disable old headers
+});
+
+app.use(limiter); // Apply rate limiting to all requests
+
+// Proxy rules
+// Rule 1: Everything that starts with /auth...
+app.use('/auth', createProxyMiddleware({
+    target: AUTH_SERVICE_URL,  // ...send it to the authentication service
+    changeOrigin: true,       
+}));
+
+// Rule 2: Everything that starts with /api...
+app.use('/api', createProxyMiddleware({
+    target: MESSAGE_SERVICE_URL, // ...send it to the message management service
+    changeOrigin: true,
+}));
+
+// Check if the gateway is running
+app.get('/gateway/health', (req, res) => {
+    res.status(200).send('API Gateway corriendo correctamente');
+});
+
+// START THE SERVER
+app.listen(PORT, () => {
+    console.log(`API Gateway corriendo en http://localhost:${PORT}`);
+});
